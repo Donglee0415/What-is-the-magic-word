c7803a27e056ecfed2b86c0b50bce9b53f1a0050
Donglee0415 <dmsloveehd0415@gmail.com>
2022-11-24 19:37:08 -0600

    create button and messages

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,39 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
  
+// Click the button to generate the paddwords
+generateBtn.addEventListener("click", function(){
+
+  let charLength = prompt("How many characters? Pick a number between 8 to 128");
+
+  if((charLength >= 8) && (charLength <= 128)){
+
+    //all possible types user can type
+    var character1 = "!@#$%^&*()[]{}~`:;?/><.,";
+    var numeric = "0123456789";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    //user confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    var genCharacter = confirm("Do you want to use special characters? ");
+    var genNumeric = confirm("Do you want to use number?");
+    var genLowerCase = confirm("Do you want to use Lowercase?");
+    var genUpperCase = confirm("Do you want to use Uppercase?");
+
+
+
+  }
+});
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
